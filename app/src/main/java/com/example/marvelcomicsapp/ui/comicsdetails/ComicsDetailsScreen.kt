4137861c9c568ae63b494c63968d96ae7a5c0ce3
Paweł Zakrzewski <pawel.zakrzewski99@gmail.com>
2023-02-20 22:51:40 +0100

    Add BottomSheetScaffold in ComicDetails

--- app/src/main/java/com/example/marvelcomicsapp/ui/comicsdetails/ComicsDetailsScreen.kt
+++ app/src/main/java/com/example/marvelcomicsapp/ui/comicsdetails/ComicsDetailsScreen.kt
@@ -1,14 +1,17 @@
 package com.example.marvelcomicsapp.ui.comicsdetails
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.background
+import androidx.compose.foundation.*
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement.SpaceBetween
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowLeft
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ScopeUpdateScope
 import androidx.compose.runtime.movableContentOf
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
@@ -18,12 +21,17 @@ import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import coil.compose.SubcomposeAsyncImage
 import coil.request.ImageRequest
+import com.example.marvelcomicsapp.R
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
@@ -38,7 +46,6 @@ fun ComicsDetailsScreen(
 
     val sheetState = rememberBottomSheetState(
         initialValue = BottomSheetValue.Collapsed
-
     )
     val bottomSheetScaffoldState = rememberBottomSheetScaffoldState(
         bottomSheetState = sheetState
@@ -46,33 +53,48 @@ fun ComicsDetailsScreen(
 
     val scope = rememberCoroutineScope()
 
+    val scroll = rememberScrollState(0)
+
 
     BottomSheetScaffold(
+        sheetShape = RoundedCornerShape(
+            topStart = 20.dp,
+            topEnd = 20.dp
+        ),
         sheetContent = {
-                       Box(modifier = Modifier
-                           .fillMaxWidth()
-                           .height(300.dp)
-                       ) {
-                           Button(onClick = {
-                               scope.launch {
-                                   if(sheetState.isCollapsed){
-                                       sheetState.expand()
-                                   } else{
-                                       sheetState.collapse()
-                                   }
-                               }
-                           }) {
-                               
-                           }
-                            Text(text = "DUPA")
-                       }
+            state.comicBook.let {
+                if (it != null) {
+                    Box( modifier = Modifier
+                        .fillMaxWidth()
+                        .height(600.dp)
+                        .background(Color.Transparent)
+                        .padding(10.dp),
+                        contentAlignment = Alignment.TopStart
+                    ){
+                        PeekScaffoldButton(
+                            scope = scope,
+                            sheetState = sheetState
+                        )
+
+                        BottomSheet(
+                            title = it.title,
+                            creators = if (it.creators.items.isEmpty()) { "Unknown" } else { it.creators.items.joinToString { creator -> creator.name } },
+                            description = if (it.description.isNullOrBlank()) { "" } else{ it.description },
+                            scroll = scroll,
+                            url = it.urls[0].url,
+                            modifier = Modifier.height(600.dp).padding(10.dp)
+                        )
+                    }
+                }
+            }
+
         },
-        sheetBackgroundColor = Color.Red,
+        sheetBackgroundColor = Color.White,
+        sheetPeekHeight = 200.dp,
         scaffoldState = bottomSheetScaffoldState
     ) {
-        Column(
-
-            modifier = Modifier.fillMaxSize()
+        Column(modifier = Modifier
+            .fillMaxSize()
         ) {
             Row(
                 modifier = Modifier
@@ -109,16 +131,106 @@ fun ComicsDetailsScreen(
                 Box(Modifier.weight(1f))
             }
             if (state.comicBook != null) {
-                val imgUrl = if (state.comicBook.images.isNotEmpty()) {
-                    "${state.comicBook.images[0].path}.${state.comicBook.images[0].extension}"
-                } else {
-                    "https://i.pinimg.com/736x/b5/34/df/b534df05c4b06ebd32159b2f9325d83f.jpg"
-                }
                 BackgroundImage(
-                    url = imgUrl,
+                    url = "${ state.comicBook.thumbnail.path }.${ state.comicBook.thumbnail.extension}" ,
                     modifier = Modifier.fillMaxSize()
                 )
             }
+        }
+    }
+}
+
+@Composable
+fun BottomSheet(
+    title: String,
+    creators: String,
+    description: String,
+    scroll: ScrollState,
+    url: String,
+    modifier: Modifier= Modifier
+){
+    Column(modifier = modifier
+        .fillMaxWidth()
+        .background(Color.Transparent)
+        .fillMaxWidth()
+        .verticalScroll(scroll),
+        verticalArrangement = SpaceBetween
+    ) {
+        Column{
+            Text(
+                text = title,
+                style = MaterialTheme.typography.h5,
+                modifier = Modifier.padding(0.dp, 7.dp),
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = creators,
+                style = MaterialTheme.typography.body1,
+                fontWeight = FontWeight.Light,
+                modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 5.dp)
+            )
+            Text(
+                text = description,
+                style = MaterialTheme.typography.h6,
+            )
+        }
+        MoreInfoButton(url)
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun PeekScaffoldButton(
+    scope: CoroutineScope,
+    sheetState: BottomSheetState
+){
+    Box(
+        modifier = Modifier
+            .fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        Box(modifier = Modifier
+            .fillMaxWidth(0.20f)
+            .height(7.5.dp)
+            .background(Color.LightGray, RoundedCornerShape(10.dp))
+            .clickable {
+                scope.launch {
+                    if (sheetState.isCollapsed) {
+                        sheetState.expand()
+                    } else {
+                        sheetState.collapse()
+                    }
+                }
+            }
+        )
+    }
+}
+@Composable
+fun MoreInfoButton(
+    url :String
+){
+    val uriHandler = LocalUriHandler.current
+    Box( modifier = Modifier
+        .fillMaxWidth()
+        .background(Color.Transparent)
+        .padding(bottom = 50.dp),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        Button(
+            onClick = {
+                uriHandler.openUri(url)
+            },
+            modifier = Modifier
+                .height(50.dp)
+                .width(250.dp),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.Red),
+            shape = RoundedCornerShape(10.dp)
+        ) {
+            Text(
+                text = "Find out more",
+                style = MaterialTheme.typography.body1,
+                color = Color.White
+            )
         }
     }
 }
