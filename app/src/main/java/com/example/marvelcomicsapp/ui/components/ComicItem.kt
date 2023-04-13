9eb2c89686ea26bd649c52507409c88b793b3b38
Paweł Zakrzewski <61842320+PawelZ4krzewski@users.noreply.github.com>
2023-04-13 09:39:56 +0200

    Chnage fonts

--- app/src/main/java/com/example/marvelcomicsapp/ui/components/ComicItem.kt
+++ app/src/main/java/com/example/marvelcomicsapp/ui/components/ComicItem.kt
@@ -3,27 +3,22 @@ package com.example.marvelcomicsapp.ui.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import com.bumptech.glide.GlideBuilder
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.example.marvelcomicsapp.R
+import com.example.marvelcomicsapp.ui.theme.*
 
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable
@@ -81,14 +76,13 @@ fun ComicInfo(
     ) {
         Text(
             text = title,
-            style = MaterialTheme.typography.h6,
-            fontWeight = FontWeight.Bold
+            style = MaterialTheme.typography.ComicTitle,
         )
         if(!author.isNullOrBlank()){
             Text(
                 text = "${stringResource(id = R.string.written_by)} $author",
-                style = MaterialTheme.typography.body2,
-                color = Color.Gray,
+                style = MaterialTheme.typography.ComicAuthorList,
+                color = Gray400,
                 maxLines = 2,
                 overflow = TextOverflow.Ellipsis,
                 softWrap = true
@@ -97,8 +91,8 @@ fun ComicInfo(
         Text(
             modifier = Modifier.padding(0.dp, 10.dp),
             text = description,
-            style = MaterialTheme.typography.body1,
-            color = Color.Gray
+            style = MaterialTheme.typography.ComicDescriptionList,
+            color = DarkGray600
         )
     }
 }
