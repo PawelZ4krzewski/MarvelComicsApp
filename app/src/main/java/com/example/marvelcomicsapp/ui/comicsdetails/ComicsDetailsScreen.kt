438f9eabe3f7db8b17338a931980911a9609d063
Paweł Zakrzewski <61842320+PawelZ4krzewski@users.noreply.github.com>
2023-04-19 09:59:52 +0200

    Add empty comics list when blank search, and change color of peek button

--- app/src/main/java/com/example/marvelcomicsapp/ui/comicsdetails/ComicsDetailsScreen.kt
+++ app/src/main/java/com/example/marvelcomicsapp/ui/comicsdetails/ComicsDetailsScreen.kt
@@ -30,10 +30,7 @@ import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.example.marvelcomicsapp.R
 import com.example.marvelcomicsapp.ui.components.HtmlText
-import com.example.marvelcomicsapp.ui.theme.ComicDescriptionList
-import com.example.marvelcomicsapp.ui.theme.ComicTitle
-import com.example.marvelcomicsapp.ui.theme.Gray400
-import com.example.marvelcomicsapp.ui.theme.Red100
+import com.example.marvelcomicsapp.ui.theme.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
@@ -217,7 +214,7 @@ fun PeekScaffoldButton(
         Box(modifier = Modifier
             .fillMaxWidth(0.20f)
             .height(7.5.dp)
-            .background(Color.LightGray, RoundedCornerShape(10.dp))
+            .background(LightGray200, RoundedCornerShape(10.dp))
             .clickable {
                 scope.launch {
                     if (sheetState.isCollapsed) {
