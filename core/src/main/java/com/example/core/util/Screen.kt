80186b15d2a35ca9b991609bd8b6063402e328fd
Paweł Zakrzewski <61842320+PawelZ4krzewski@users.noreply.github.com>
2023-05-02 08:27:31 +0200

    Add fav comics to bottom bar menu

--- core/src/main/java/com/example/core/util/Screen.kt
+++ core/src/main/java/com/example/core/util/Screen.kt
@@ -4,4 +4,5 @@ sealed class Screen(val route: String) {
     object SearchComicListScreen : Screen("search_comics_screen")
     object ComicListScreen : Screen("comic_list_screen")
     object ComicsDetailsScreen : Screen("comics_details")
+    object FavComicsScreen : Screen("fav_comics_details")
 }
